a4a4fd7512be69350c02232fa42039df8303022a
SecretKeeper <secretk33p3r@gmail.com>
2023-02-09 14:34:53 +0330

    create message.repository.ts

---
+++ src/events/message/message.repository.ts
@@ -0,0 +1,34 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { mapping } from 'cassandra-driver';
+import { CassandraService } from 'src/core/cassandra/cassandra.service';
+import { Message } from './message.model';
+
+@Injectable()
+export class MessageRepository implements OnModuleInit {
+  constructor(private cassandraService: CassandraService) {}
+
+  messageMapper: mapping.ModelMapper<Message>;
+
+  onModuleInit() {
+    const mappingOptions: mapping.MappingOptions = {
+      models: {
+        Message: {
+          tables: ['messages'],
+          mappings: new mapping.UnderscoreCqlToCamelCaseMappings(),
+        },
+      },
+    };
+
+    this.messageMapper = this.cassandraService
+      .createMapper(mappingOptions)
+      .forModel('Message');
+  }
+
+  async getMessages() {
+    return (await this.messageMapper.findAll()).toArray();
+  }
+
+  async createMessage(message: Message) {
+    return (await this.messageMapper.insert(message)).toArray();
+  }
+}
