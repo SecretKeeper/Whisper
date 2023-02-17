c4443189f0e80f14934c44b8d2efe21fda98dd78
SecretKeeper <secretk33p3r@gmail.com>
2023-02-17 17:40:05 +0330

    push latest changes

--- src/events/message/message.gateway.ts
+++ src/events/message/message.gateway.ts
@@ -1,40 +1,47 @@
+import { BadRequestTransformationFilter } from '@core/filters/ws-bad-request.filter';
+import { UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
 import {
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  WsResponse,
 } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Server } from 'ws';
+import { CreateMessageDTO } from './dto/create-message.dto';
 import { MessageService } from './message.service';
 
+@UseFilters(BadRequestTransformationFilter)
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class MessagesGateway {
+export class MessagesGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private messageService: MessageService) {}
 
-  @WebSocketServer()
-  @SubscribeMessage('events')
-  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-    return from([1, 2, 3]).pipe(
-      map((item) => ({ event: 'events', data: item })),
-    );
+  @WebSocketServer() server: Server;
+
+  handleDisconnect(client: any) {
+    console.log('handleDisconnect', client);
   }
 
+  handleConnection(client: any, ...args: any[]) {
+    console.log('handleConnection', client);
+  }
+
+  afterInit(server: Server) {
+    console.log('SERVER IS ', server.clients);
+  }
+
+  @UsePipes(new ValidationPipe())
   @SubscribeMessage('identity')
-  async identity(@MessageBody() data: number): Promise<number> {
-    this.messageService.createMessage({
-      id: 'QQ',
-      // local_id: 'QQQ',
-      // content: 'QQQQQQ',
-      // sender: 'QQQQSender',
-      // receiver: 'QQQReceiver',
-      // created_at: 'QWEQW@#2313',
-    });
-    return data * 2;
+  async identity(@MessageBody() message: CreateMessageDTO): Promise<number> {
+    this.messageService.createMessage(message);
+    return 2;
   }
 }
