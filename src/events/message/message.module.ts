7c6b1dd2d82798fa295f5fa4036d06c25bff10f6
SecretKeeper <secretk33p3r@gmail.com>
2023-02-20 09:35:22 +0330

    inject NATS and MessageController

--- src/events/message/message.module.ts
+++ src/events/message/message.module.ts
@@ -4,15 +4,30 @@ import { CassandraModule } from '@core/cassandra/cassandra.module';
 import { MessagesGateway } from './message.gateway';
 import { MessageRepository } from './message.repository';
 import { MessageService } from './message.service';
+import { MessageController } from './message.controller';
+import { PhantomService } from '@core/phantoms/phantom.service';
 
 @Module({
   imports: [
     ClientsModule.register([
       { name: 'AUTH_MICROSERVICE', transport: Transport.TCP },
+      {
+        name: 'NATS',
+        transport: Transport.NATS,
+        options: {
+          servers: ['nats://localhost:4222'],
+        },
+      },
     ]),
     CassandraModule,
   ],
-  providers: [MessagesGateway, MessageService, MessageRepository],
+  providers: [
+    MessagesGateway,
+    MessageService,
+    MessageRepository,
+    PhantomService,
+  ],
+  controllers: [MessageController],
   exports: [MessageRepository],
 })
 export class MessageModule {}
