7fa3edeea769c03df878f506b6920376497fa42e
SecretKeeper <secretk33p3r@gmail.com>
2023-03-03 18:48:33 +0330

    added conversations table , and updated messages table with new schema

--- migration.py
+++ migration.py
@@ -7,14 +7,31 @@ cluster = Cluster(['127.0.0.1'], port=9042, auth_provider=auth_provider)
 session = cluster.connect('messages')
 
 session.execute(
+    """
+        CREATE TABLE IF NOT EXISTS conversations (
+          conversation_id uuid,
+          created_at timestamp,
+          participants set<uuid>,
+          PRIMARY KEY (conversation_id)
+        )
+        WITH compression = {'sstable_compression': 'LZ4Compressor'};
+    """
+)
+
+session.execute(
 	"""
-	CREATE TABLE messages (
+	CREATE TABLE IF NOT EXISTS messages (
 		id uuid,
-		sender text,
-		receiver text,
+		conversation_id uuid,
+		sender_id uuid,
+		recipient_id uuid,
 		content text,
+		seen_at timestamp,
 		created_at timestamp,
-		PRIMARY KEY ((sender, receiver), created_at)
-	) WITH CLUSTERING ORDER BY (created_at DESC);
+		updated_at timestamp,
+		PRIMARY KEY ((conversation_id), created_at, seen_at, id)
+	)
+	WITH CLUSTERING ORDER BY (created_at DESC)
+	AND compression = {'sstable_compression': 'LZ4Compressor'};
 	"""
 )
