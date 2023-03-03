5d7da9830d1df474505f31aa78d71a8561806cdb
SecretKeeper <secretk33p3r@gmail.com>
2023-03-03 19:35:02 +0330

    updated sender_id receiver_id to uuid and renamed with new changes, phantom id also store as uuid instead of string

--- src/core/phantoms/phantom.service.ts
+++ src/core/phantoms/phantom.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { types } from 'cassandra-driver';
 
 @Injectable()
 export class PhantomService {
-  phantoms: Map<string, any> = new Map();
+  phantoms: Map<types.Uuid, any> = new Map();
 
-  public connectPhantom(phantom_id: string, client: any): void {
+  public connectPhantom(phantom_id: types.Uuid, client: any): void {
     this.phantoms.set(phantom_id, client);
   }
 
-  public disconnectPhantom(phantom_id: string): void {
+  public disconnectPhantom(phantom_id: types.Uuid): void {
     this.phantoms.delete(phantom_id);
   }
 }
